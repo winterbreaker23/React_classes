1a81573de8fa53a0693c194813d1eea7ef9c4fe4
winterbreaker23 <johnjamesdev@hotmail.com>
2023-05-31 15:50:21 +0100

    exp1

---
+++ myapp/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+class Car extends React.Component{
+  render(){
+    return <h1>This is a Car.</h1>
+  }
+}
+root.render(
+  <React.StrictMode>
+    <Car />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
