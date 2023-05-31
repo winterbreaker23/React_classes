05f14441dc12c6d428e34252c343bfc5242c05b6
winterbreaker23 <johnjamesdev@hotmail.com>
2023-05-31 17:05:07 +0100

    componentWillUnmount

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -168,9 +168,42 @@ class Header5 extends React.Component {
     );
   }
 }
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {show: true};
+  }
+  delHeader = () => {
+    this.setState({show: false});
+  }
+  render() {
+    let myheader;
+    if (this.state.show) {
+      myheader = <Child />;
+    };
+    return (
+      <div>
+      {myheader}
+      <button type="button" onClick={this.delHeader}>Delete Header</button>
+      </div>
+    );
+  }
+}
+
+class Child extends React.Component {
+  componentWillUnmount() {
+    alert("The component named Header is about to be unmounted.");
+  }
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
 root.render(
   <React.StrictMode>
-    <Header5/>
+    <Container/>
   </React.StrictMode>
 );
 
