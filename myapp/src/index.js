0ffceb90401f25bd8f8600817309ab050d3a24c6
winterbreaker23 <johnjamesdev@hotmail.com>
2023-05-31 16:52:50 +0100

    shouldComponentUpdate

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -117,9 +117,32 @@ class Header3 extends React.Component{
     );
   }
 }
+
+class Header4 extends React.Component{
+
+    constructor(props) {
+      super(props);
+      this.state = {favoritecolor: "red"};
+    }
+    shouldComponentUpdate() {
+      return false;
+    }
+    changeColor = () => {
+      this.setState({favoritecolor: "blue"});
+    }
+    render() {
+      return (
+        <div>
+        <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+        <button type="button" onClick={this.changeColor}>Change color</button>
+        </div>
+      );
+    }
+  
+}
 root.render(
   <React.StrictMode>
-    <Header3 favcol="yellow"/>
+    <Header4/>
   </React.StrictMode>
 );
 
